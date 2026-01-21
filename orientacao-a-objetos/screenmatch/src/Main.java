19ff70f824a8405cafdf15e6efd8ebaea797ac3f
Hugo Luis <hugolcabreu@gmail.com>
2026-01-21 01:52:10 -0300

    Introduz conceitos de modificadores de acesso, imports, this, getters e setters (módulo 2 do curso)

--- orientacao-a-objetos/screenmatch/src/Main.java
+++ orientacao-a-objetos/screenmatch/src/Main.java
@@ -1,3 +1,5 @@
+import modelos.Filme;
+
 public class Main {
     public static void main(String[] args) throws Exception {
         Filme meuFilme = new Filme("Django Livre", "2013", true, 165);
@@ -5,8 +7,8 @@ public class Main {
         meuFilme.avalia(8);
         meuFilme.avalia(10);
         meuFilme.avalia(9);
-        System.out.println(meuFilme.somaDasAvaliacoes);
-        System.out.println(meuFilme.totalAvaliacoes);
+        System.out.println(meuFilme.getSomaDasAvaliacoes());
+        System.out.println(meuFilme.getTotalAvaliacoes());
         System.out.println(meuFilme.pegaMedia());
     }
 }
