b0cb0aa3bb1d3a7271be7b37b7545599e6139694
Hugo Luis <hugolcabreu@gmail.com>
2026-01-24 18:24:40 -0300

    Implementa arrayList

--- screenmatch/src/Main.java
+++ screenmatch/src/Main.java
@@ -1,18 +1,11 @@
+import java.util.ArrayList;
+
 import calculos.FiltroRecomendacao;
 import modelos.Filme;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Filme meuFilme = new Filme("Django Livre", "2013", true, 165, "Tarantino");
-        meuFilme.exibeFichaTecnica();
-        meuFilme.avalia(8);
-        meuFilme.avalia(10);
-        meuFilme.avalia(9);
-        System.out.println(meuFilme.getSomaDasAvaliacoes());
-        System.out.println(meuFilme.getTotalAvaliacoes());
-        System.out.println(meuFilme.pegaMedia());
-
-        FiltroRecomendacao meuFiltro = new FiltroRecomendacao();
-        meuFiltro.filtra(meuFilme);
+        ArrayList <Filme> filmes = new ArrayList<>();
+        
     }
 }
