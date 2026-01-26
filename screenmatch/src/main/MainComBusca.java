3568c5bda1b8dc08b8036090bbb76a832ed32edb
Hugo Luis <hugolcabreu@gmail.com>
2026-01-26 19:07:04 -0300

    Adiciona exercicio sobre API REST google books

---
+++ screenmatch/src/main/MainComBusca.java
@@ -0,0 +1,24 @@
+package main;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.Scanner;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+
+public class MainComBusca {
+    public static void main(String[] args) throws IOException, InterruptedException{
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite um filme para busca: ");
+        var busca = sc.nextLine();
+        String endereco = "http://www.omdbapi.com/?t=" + busca + "&apikey=6490763c";
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endereco))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, BodyHandlers.ofString());
+        System.out.println(response.body());
+    }
+}
